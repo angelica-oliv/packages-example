b3c137755d1c3124069bf709f4b7660d1cef5338
Angélica Oliveira <angelicaoliveira@LAangelicadeoliveira.local>
2019-11-17 17:50:01 -0300

    Packages first commit

---
+++ packages-definition/src/test/kotlin/com/angelicao/packages/PackageTextTest.kt
@@ -0,0 +1,13 @@
+package com.angelicao.packages
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class PackageTextTest {
+    @Test
+    fun packageStr_checkTextIsCorrect() {
+        val packageText = PackageText()
+
+        assertEquals("Hello I am a text in packages!", packageText.packageStr)
+    }
+}
